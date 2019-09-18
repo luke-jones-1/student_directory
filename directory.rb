6cc660b6c7f326369f73af6a2bbb035b7cb8d1f4
Luke Jones <lukejonesice@yahoo.co.uk>
2019-09-18 15:31:17 +0100

    Change

--- directory.rb
+++ directory.rb
@@ -1,13 +1,29 @@
-puts """
-The students of Villains academy
----------------
-Sauron
-Tom Riddle
-Boris Johnson
-Bowser
-Alduin
-Night Mother
-Comcast
-My mum
-The System
-"""
+
+students = [
+            {name: "Sauron", cohort: :november},
+            {name: "Tom Riddle", cohort: :november},
+            {name: "Boris Johnson", cohort: :november},
+            {name: "Bowser", cohort: :november},
+            {name: "Alduin", cohort: :november},
+            {name: "Night Mother", cohort: :november},
+            {name: "Comcast", cohort: :november},
+            {name: "My mum", cohort: :november},
+            {name: "The System", cohort: :november},
+]
+
+def header
+  puts """The students of Villains academy
+  ---------------"""
+end
+
+def print(students)
+students.each {|students| puts "#{students[:name]} (#{students[:cohort]} cohort)"}
+end
+
+def footer(names)
+puts "Overall, we have #{names.count} great students."
+end
+
+header()
+print(students)
+footer(students)
